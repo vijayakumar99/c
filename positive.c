b0e1c78d570c40077bfd1a26db1ac8c7966905d5
vijayakumar99 <vijaykumar43384@gmail.com>
2016-07-14 11:50:54 +0530

    Update positive.c

--- positive.c
+++ positive.c
@@ -1,1 +1,15 @@
-
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+  int a;
+  scanf("%d",&a);
+  if(a>0)
+  {
+    printf("positive number");
+  }
+  else
+  {
+    printf("negative number");
+  }
+}
