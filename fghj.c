dc2abfea4d78db6dcd3cfb18f7688c3176359412
vijayakumar99 <vijaykumar43384@gmail.com>
2016-08-30 14:58:20 +0530

    fghj.c

---
+++ fghj.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+ 
+void main() {
+   int n;
+   char i[3];
+ 
+   printf("Please Enter String : ");
+   scanf("%s", i);
+ 
+   n = atoi(i);
+   printf("\n Integer is : %d", n);
+ 
+  
+}
